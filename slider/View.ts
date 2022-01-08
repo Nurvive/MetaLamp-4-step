c907dd7168c5bb5bb2ac0d76000eb9c2d916ebed
Nurvive <jiimbo689@gmail.com>
2022-01-09 01:29:53 +0700

    Вынес функцию swipeAction из swipeStart для удобного тестирования

--- slider/View.ts
+++ slider/View.ts
@@ -5,18 +5,18 @@ import Line from './subView/Line';
 import {notifyData} from './Model';
 
 interface state {
-    min: number
-    max: number
-    step: number
-    direction: string
-    type: string
-    valueFrom: number
-    valueTo: number
-    bubble: boolean
-    onChangeTo: (value: number) => void
-    onChangeFrom: (value: number) => void
+    min: number;
+    max: number;
+    step: number;
+    direction: string;
+    type: string;
+    valueFrom: number;
+    valueTo: number;
+    bubble: boolean;
+    onChangeTo: (value: number) => void;
+    onChangeFrom: (value: number) => void;
 
-    [key: string]: string | number | boolean | undefined | ((value: number) => void)
+    [key: string]: string | number | boolean | undefined | ((value: number) => void);
 }
 
 export class View extends Observer {
@@ -32,9 +32,12 @@ export class View extends Observer {
 
     head2?: ViewHead;
 
+    swipeHandler: (event: (MouseEvent | TouchEvent)) => Array<number>;
+
     constructor(elem: HTMLElement) {
         super();
         this.elem = elem;
+        this.swipeHandler = () => [];
         this.state = {
             bubble: true,
             max: 100,
@@ -82,11 +85,13 @@ export class View extends Observer {
         this.setup();
     }
 
-    protected setup(): void {
+    setup(): void {
         if (this.state.type === 'double') {
             if (this.head2 !== undefined) {
                 this.head2.element.addEventListener('mousedown', this.swipeStart);
                 this.head2.element.addEventListener('touchstart', this.swipeStart);
+            } else {
+                throw new Error('Head2 не существует');
             }
         }
         this.head.element.addEventListener('mousedown', this.swipeStart);
@@ -105,7 +110,7 @@ export class View extends Observer {
         return event;
     }
 
-    swipeStart = (e: MouseEvent | TouchEvent): void => {
+    swipeStart = (e: MouseEvent | TouchEvent): Array<number> => {
         const evt: MouseEvent | Touch = View.getEvent(e);
         const target = evt.target as Element;
         const updatedHead: string = target.hasAttribute('data-valueFrom') ? 'valueFrom' : 'valueTo';
@@ -117,53 +122,61 @@ export class View extends Observer {
             dataArray.push(target.getBoundingClientRect().top);
             dataArray.push(evt.clientY);
         }
-        const swipeAction = (event: MouseEvent | TouchEvent): void => {
-            if ('preventDefault' in event) {
-                event.preventDefault();
-            }
-            const evtSwipe: MouseEvent | Touch = View.getEvent(event);
-            if (this.state.direction === 'horizontal') {
-                dataArray.push(this.line.getWidth);
-                dataArray.push(this.line.getLeftCoordinate);
-                dataArray.push(evtSwipe.clientX);
-            } else {
-                dataArray.push(this.line.getHeight);
-                dataArray.push(this.line.getTopCoordinate);
-                dataArray.push(evtSwipe.clientY);
-            }
-            dataArray.push(this.head.getWidth / 2);
-            this.notify({
-                valueArr: dataArray.slice(),
-                target: updatedHead,
-                onlyState: false
-            });
-            dataArray.splice(2, dataArray.length - 2);
+        this.swipeHandler = (event: MouseEvent | TouchEvent): Array<number> => {
+            return this.swipeAction(event, dataArray, updatedHead);
         };
-        const swipeEnd = (): void => {
-            document.removeEventListener('touchmove', swipeAction);
-            document.removeEventListener('mousemove', swipeAction);
-            document.removeEventListener('touchend', swipeEnd);
-            document.removeEventListener('mouseup', swipeEnd);
-        };
-        document.addEventListener('touchmove', swipeAction, {passive: false});
-        document.addEventListener('mousemove', swipeAction);
-        document.addEventListener('touchend', swipeEnd);
-        document.addEventListener('mouseup', swipeEnd);
-    }
+        document.addEventListener('touchmove', this.swipeHandler, {passive: false});
+        document.addEventListener('mousemove', this.swipeHandler);
+        document.addEventListener('touchend', this.swipeEnd);
+        document.addEventListener('mouseup', this.swipeEnd);
+        return dataArray;
+    };
 
-    onLineClick(event: MouseEvent): void {
-        const dataArray: Array<number> = this.calcLineClickPositionRelative(event);
+    swipeAction = (event: MouseEvent | TouchEvent, dataArray: Array<number>, updatedHead: string):
+        Array<number> => {
+        event.preventDefault();
+        const evtSwipe: MouseEvent | Touch = View.getEvent(event);
+        if (this.state.direction === 'horizontal') {
+            dataArray.push(this.line.getWidth);
+            dataArray.push(this.line.getLeftCoordinate);
+            dataArray.push(evtSwipe.clientX);
+        } else {
+            dataArray.push(this.line.getHeight);
+            dataArray.push(this.line.getTopCoordinate);
+            dataArray.push(evtSwipe.clientY);
+        }
+        dataArray.push(this.head.getWidth / 2);
+        this.notify({
+            valueArr: dataArray.slice(),
+            target: updatedHead,
+            onlyState: false
+        });
+        dataArray.splice(2, dataArray.length - 2);
+        return dataArray;
+    };
+
+    swipeEnd = (): boolean => {
+        document.removeEventListener('touchmove', this.swipeHandler);
+        document.removeEventListener('mousemove', this.swipeHandler);
+        document.removeEventListener('touchend', this.swipeEnd);
+        document.removeEventListener('mouseup', this.swipeEnd);
+        return true;
+    };
+
+    onLineClick(event: MouseEvent): Array<number> {
+        const dataArray: Array<number> = this.lineClickData(event);
 
         this.notify({
             target: 'value',
             valueArr: dataArray.slice(),
             onlyState: false
         });
+        return dataArray;
     }
 
-    calcLineClickPositionRelative(event: MouseEvent | TouchEvent): Array<number> {
+    lineClickData(event: MouseEvent | TouchEvent): Array<number> {
         const evt: MouseEvent | Touch = View.getEvent(event);
-        const dataArray:Array<number> = [];
+        const dataArray: Array<number> = [];
         if (this.state.direction === 'horizontal') {
             dataArray.push(this.line.getWidth);
             dataArray.push(this.line.getLeftCoordinate);
@@ -181,6 +194,8 @@ export class View extends Observer {
         let position = 0;
         if (data.valueN !== undefined) {
             position = data.valueN;
+        } else {
+            throw new Error('Новое значение не определено');
         }
         if (data.target === 'valueTo') {
             this.head.updatePosition(position);
@@ -191,45 +206,79 @@ export class View extends Observer {
             if (this.head2 !== undefined) {
                 this.head2.updatePosition(position);
                 this.head2.updateBubble(this.state.valueFrom);
+                this.state.onChangeFrom(this.state.valueFrom);
+                this.line.progressValue(this.head.element, this.head2.element);
+            } else {
+                throw new Error('Head2 не существует');
             }
-            this.state.onChangeFrom(this.state.valueFrom);
-            this.line.progressValue(this.head.element, this.head2?.element);
         }
     }
 
     hideBubble(): void {
-        this.updateState({target: 'bubble', valueB: false, onlyState: true});
+        this.updateState({
+            target: 'bubble',
+            valueB: false,
+            onlyState: true
+        });
         this.head.hideBubble();
         this.head2?.hideBubble();
-        this.notify({target: 'bubble', valueB: false, onlyState: true});
+        this.notify({
+            target: 'bubble',
+            valueB: false,
+            onlyState: true
+        });
     }
 
     showBubble(): void {
-        this.updateState({target: 'bubble', valueB: true, onlyState: true});
+        this.updateState({
+            target: 'bubble',
+            valueB: true,
+            onlyState: true
+        });
         this.head.showBubble();
         this.head2?.showBubble();
-        this.notify({target: 'bubble', valueB: true, onlyState: true});
+        this.notify({
+            target: 'bubble',
+            valueB: true,
+            onlyState: true
+        });
     }
 
     changeOrientation(value: string): void {
-        this.updateState({target: 'direction', valueS: value, onlyState: true});
+        this.updateState({
+            target: 'direction',
+            valueS: value,
+            onlyState: true
+        });
         this.head.removeHead();
         this.head2?.removeHead();
         this.scale.removeScale();
         this.line.removeLine();
         this.init({});
-        this.notify({target: 'direction', valueS: value, onlyState: true});
+        this.notify({
+            target: 'direction',
+            valueS: value,
+            onlyState: true
+        });
     }
 
     changeType(value: string): void {
-        this.updateState({target: 'type', valueS: value, onlyState: true});
+        this.updateState({
+            target: 'type',
+            valueS: value,
+            onlyState: true
+        });
         this.head.removeHead();
         this.head2?.removeHead();
         delete this.head2;
         this.scale.removeScale();
         this.line.removeLine();
         this.init({});
-        this.notify({target: 'type', valueS: value, onlyState: true});
+        this.notify({
+            target: 'type',
+            valueS: value,
+            onlyState: true
+        });
     }
 
     set changeStep(value: number) {
@@ -241,7 +290,11 @@ export class View extends Observer {
         if (value < this.state.min || value <= this.state.valueFrom) {
             throw new Error('Максимум не может быть меньше минимума');
         }
-        this.updateState({target: 'max', valueN: value, onlyState: true});
+        this.updateState({
+            target: 'max',
+            valueN: value,
+            onlyState: true
+        });
         this.head.removeHead();
         this.head2?.removeHead();
         this.scale.removeScale();
@@ -253,7 +306,11 @@ export class View extends Observer {
         if (value > this.state.max || value >= this.state.valueTo) {
             throw new Error('Минимум не может быть больше максимума');
         }
-        this.updateState({target: 'min', valueN: value, onlyState: true});
+        this.updateState({
+            target: 'min',
+            valueN: value,
+            onlyState: true
+        });
         this.head.removeHead();
         this.head2?.removeHead();
         this.scale.removeScale();
