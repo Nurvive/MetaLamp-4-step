b6a40a6a9c4ea2d73469cd702421d2ea74724819
Nurvive <jiimbo689@gmail.com>
2022-01-09 13:53:02 +0700

    Убрал ненужные поля в Presenter

--- slider/Presenter.ts
+++ slider/Presenter.ts
@@ -2,18 +2,12 @@ import {Model} from './Model';
 import {View} from './View';
 
 export class Presenter {
-    elem: HTMLElement;
-
     model: Model;
-
-    elemHead: HTMLElement | null;
 
     view: View
 
     constructor(elem: HTMLElement, model: Model, view: View) {
-        this.elem = elem;
         this.model = model;
-        this.elemHead = document.getElementById('slider__head');
         this.view = view;
         this.view.subscribe(this.model.updateState.bind(this.model));
         this.view.subscribe(this.model.calcPosition.bind(this.model));
