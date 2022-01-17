5d9ce151e133fd6f9118b91f727a5297f1b3b1ff
Nurvive <jiimbo689@gmail.com>
2022-01-17 20:04:11 +0700

    Переписал index.js на ts

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import './styles.scss';
 import './slider/jquery.slider';
 
-$().ready(function () {
+$(function () {
     const $bubbleButton = $('input[data-type="bubble"]');
     const $verticalButton = $('input[data-type="vertical"]');
     const $rangeButton = $('input[data-type="range"]');
@@ -19,37 +19,37 @@ $().ready(function () {
         type: 'double',
         valueTo: 100,
         valueFrom: 0,
-        onChangeTo: function (value) {
+        onChangeTo: function (value: number) {
             $toInput.val(value);
         },
-        onChangeFrom: function (value) {
+        onChangeFrom: function (value: number) {
             $fromInput.val(value);
         }
     });
 
     $bubbleButton.on('click', function () {
-        this.checked ? slider1.Slider('showBubble') : slider1.Slider('hideBubble');
+        (this as HTMLInputElement).checked ? slider1.Slider('showBubble') : slider1.Slider('hideBubble');
     });
     $verticalButton.on('click', function () {
-        this.checked ? slider1.Slider('changeOrientation', 'vertical') : slider1.Slider('changeOrientation', 'horizontal');
+        (this as HTMLInputElement).checked ? slider1.Slider('changeOrientation', 'vertical') : slider1.Slider('changeOrientation', 'horizontal');
     });
     $rangeButton.on('click', function () {
-        this.checked ? slider1.Slider('changeType', 'double') : slider1.Slider('changeType', 'single');
+        (this as HTMLInputElement).checked ? slider1.Slider('changeType', 'double') : slider1.Slider('changeType', 'single');
     });
     $stepInput.on('input', function () {
-        slider1.Slider('changeStep', parseFloat(this.value));
+        slider1.Slider('changeStep', parseFloat((this as HTMLInputElement).value));
     });
     $toInput.on('input', function () {
-        slider1.Slider('changeTo', parseFloat(this.value));
+        slider1.Slider('changeTo', parseFloat((this as HTMLInputElement).value));
     });
     $fromInput.on('input', function () {
-        slider1.Slider('changeFrom', parseFloat(this.value));
+        slider1.Slider('changeFrom', parseFloat((this as HTMLInputElement).value));
     });
     $maxInput.on('input', function () {
-        slider1.Slider('changeMax', parseFloat(this.value));
+        slider1.Slider('changeMax', parseFloat((this as HTMLInputElement).value));
     });
     $minInput.on('input', function () {
-        slider1.Slider('changeMin', parseFloat(this.value));
+        slider1.Slider('changeMin', parseFloat((this as HTMLInputElement).value));
     });
     const $bubbleButton2 = $('input[data-type="bubble2"]');
     const $verticalButton2 = $('input[data-type="vertical2"]');
@@ -68,36 +68,36 @@ $().ready(function () {
         type: 'single',
         valueTo: 50,
         valueFrom: 15,
-        onChangeTo: function (value) {
+        onChangeTo: function (value: number) {
             $toInput2.val(value);
         },
-        onChangeFrom: function (value) {
+        onChangeFrom: function (value: number) {
             $fromInput2.val(value);
         }
     });
     $bubbleButton2.on('click', function () {
-        this.checked ? slider2.Slider('showBubble') : slider2.Slider('hideBubble');
+        (this as HTMLInputElement).checked ? slider2.Slider('showBubble') : slider2.Slider('hideBubble');
     });
     $verticalButton2.on('click', function () {
-        this.checked ? slider2.Slider('changeOrientation', 'vertical') : slider2.Slider('changeOrientation', 'horizontal');
+        (this as HTMLInputElement).checked ? slider2.Slider('changeOrientation', 'vertical') : slider2.Slider('changeOrientation', 'horizontal');
     });
     $rangeButton2.on('click', function () {
-        this.checked ? slider2.Slider('changeType', 'double') : slider2.Slider('changeType', 'single');
+        (this as HTMLInputElement).checked ? slider2.Slider('changeType', 'double') : slider2.Slider('changeType', 'single');
     });
     $stepInput2.on('input', function () {
-        slider2.Slider('changeStep', parseInt(this.value, 10));
+        slider2.Slider('changeStep', parseInt((this as HTMLInputElement).value, 10));
     });
     $toInput2.on('input', function () {
-        slider2.Slider('changeTo', parseInt(this.value, 10));
+        slider2.Slider('changeTo', parseInt((this as HTMLInputElement).value, 10));
     });
     $fromInput2.on('input', function () {
-        slider2.Slider('changeFrom', parseInt(this.value, 10));
+        slider2.Slider('changeFrom', parseInt((this as HTMLInputElement).value, 10));
     });
     $maxInput2.on('input', function () {
-        slider2.Slider('changeMax', parseInt(this.value, 10));
+        slider2.Slider('changeMax', parseInt((this as HTMLInputElement).value, 10));
     });
     $minInput2.on('input', function () {
-        slider2.Slider('changeMin', parseInt(this.value, 10));
+        slider2.Slider('changeMin', parseInt((this as HTMLInputElement).value, 10));
     });
     const $bubbleButton3 = $('input[data-type="bubble3"]');
     const $verticalButton3 = $('input[data-type="vertical3"]');
@@ -116,36 +116,36 @@ $().ready(function () {
         type: 'single',
         valueTo: 780,
         valueFrom: -5,
-        onChangeTo: function (value) {
+        onChangeTo: function (value: number) {
             $toInput3.val(value);
         },
-        onChangeFrom: function (value) {
+        onChangeFrom: function (value: number) {
             $fromInput3.val(value);
         }
     });
 
     $bubbleButton3.on('click', function () {
-        this.checked ? slider3.Slider('showBubble') : slider3.Slider('hideBubble');
+        (this as HTMLInputElement).checked ? slider3.Slider('showBubble') : slider3.Slider('hideBubble');
     });
     $verticalButton3.on('click', function () {
-        this.checked ? slider3.Slider('changeOrientation', 'vertical') : slider3.Slider('changeOrientation', 'horizontal');
+        (this as HTMLInputElement).checked ? slider3.Slider('changeOrientation', 'vertical') : slider3.Slider('changeOrientation', 'horizontal');
     });
     $rangeButton3.on('click', function () {
-        this.checked ? slider3.Slider('changeType', 'double') : slider3.Slider('changeType', 'single');
+        (this as HTMLInputElement).checked ? slider3.Slider('changeType', 'double') : slider3.Slider('changeType', 'single');
     });
     $stepInput3.on('input', function () {
-        slider3.Slider('changeStep', parseInt(this.value, 10));
+        slider3.Slider('changeStep', parseInt((this as HTMLInputElement).value, 10));
     });
     $toInput3.on('input', function () {
-        slider3.Slider('changeTo', parseInt(this.value, 10));
+        slider3.Slider('changeTo', parseInt((this as HTMLInputElement).value, 10));
     });
     $fromInput3.on('input', function () {
-        slider3.Slider('changeFrom', parseInt(this.value, 10));
+        slider3.Slider('changeFrom', parseInt((this as HTMLInputElement).value, 10));
     });
     $maxInput3.on('input', function () {
-        slider3.Slider('changeMax', parseInt(this.value, 10));
+        slider3.Slider('changeMax', parseInt((this as HTMLInputElement).value, 10));
     });
     $minInput3.on('input', function () {
-        slider3.Slider('changeMin', parseInt(this.value, 10));
+        slider3.Slider('changeMin', parseInt((this as HTMLInputElement).value, 10));
     });
 });
